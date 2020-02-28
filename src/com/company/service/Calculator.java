054a0ca001dc4102fb828e2c169f5499ca631f31
Maxorax <37916430+maxorax@users.noreply.github.com>
2020-02-29 00:39:31 +0400

    Update Calculator.java

--- src/com/company/service/Calculator.java
+++ src/com/company/service/Calculator.java
@@ -10,26 +10,33 @@ public class Calculator extends ParseStringtoExpression {
 
     public  void Answer(String expr) throws NotCorrectExpression {
         List<String> rpn = ExpressionToReservePolishNotation(expr);
+       String expression= String.valueOf(rpn).replaceAll(", ","");
+        expression=expression.replaceAll("\\[","");
+        expression=expression.replaceAll("]","");
         Stack<Double> stack = new Stack<>();
         String answer = new String();
-        for (int i = 0;i < rpn.size();i++){
-            if(getPriority(rpn.get(i).charAt(0)) == 0){
-                answer += rpn.get(i);
+        for (int i = 0;i < expression.length();i++){
+            if(expression.charAt(i)== ' ') continue;
+            if(getPriority(expression.charAt(i)) == 0){
+                while (getPriority(expression.charAt(i)) == 0 && expression.charAt(i)!= ' ' ){
+                    answer+=expression.charAt(i++);
+                    if(i==expression.length()) break;
+                }
                 stack.push(Double.parseDouble(answer));
                 answer = new String();
             }
 
-            if(getPriority(rpn.get(i).charAt(0))>1){
+            if(getPriority(expression.charAt(i))>1  ){
                 double a= stack.pop();
 
                 double b= stack.pop();
-                if(rpn.get(i).charAt(0) == '+'){
+                if(expression.charAt(i) == '+'){
                     stack.push(b+a);
-                } else if(rpn.get(i).charAt(0) == '-'){
+                } else if(expression.charAt(i) == '-'){
                     stack.push(b-a);
-                } else if(rpn.get(i).charAt(0) == '*'){
+                } else if(expression.charAt(i) == '*'){
                     stack.push(b*a);
-                } else if(rpn.get(i).charAt(0) == '/' && a !=0){
+                } else if(expression.charAt(i) == '/' && a !=0){
                     stack.push(b/a);
                 } else{
                     throw new NullPointerException("Деление на ноль невозможно");
